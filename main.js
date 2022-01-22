c02180480e450a7e84cd62efdad6df37ca65d4c2
bsandue <zh.s.beisbaevy@mail.ru>
2022-01-22 14:17:43 +0600

    feat:added mathematical operations

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 function calc(c,  a, b){
 	if ( typeof a !== 'number' || typeof b !== 'number'){
-		return 'Error';}
+		return 'Error';
+	} else if (c == '+') {
+		return a + b;
 	}
+	else if (c == '-') {
+		return a - b;
+	}
+	else if (c == '/') {
+		return a / b;
+	}
+	else if (c == '*') {
+		return a * b;
+	}
+	else if (c == '%') {
+		return a % b;
+	}
+	else if (c == '**') { 
+		return a ** b;
+	}
+}
